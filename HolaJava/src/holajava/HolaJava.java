032132ba4e843d751476733060187dd14ba33c80
evil5 <evil5@192.168.0.23>
2018-03-09 21:30:09 -0600

    Cambios en el código

--- HolaJava/src/holajava/HolaJava.java
+++ HolaJava/src/holajava/HolaJava.java
@@ -15,7 +15,7 @@ public class HolaJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola Java");
     }
     
 }
