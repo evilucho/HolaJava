48cd01184672e2ab7abed91460cc18fd3f3ea4e5
Eleazar Morales <evil5464@hotmail.com>
2018-03-10 12:20:18 -0600

    Modificación directa sobre el máster para ver como se actualiza en los forks

--- HolaJava/src/holajava/HolaJava.java
+++ HolaJava/src/holajava/HolaJava.java
@@ -12,6 +12,7 @@ public class HolaJava {
         Scanner lee = new Scanner(System.in);
         //hacemos una modificación
         //relizamos entonces una mejora del sludo.
+        //Edición directa para ver como se actualizan los cambios
         System.out.println("Ingresa tu nombre...\n");
         nombre =lee.nextLine();
         System.out.println("Hola Java");
